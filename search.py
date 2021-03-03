394a697ef0b0342716cc78d016c1dc03dc87efdd
tanchuanxin <chuanxin2008@hotmail.com>
2021-03-04 04:07:15 +0800

    prep for search

--- search.py
+++ search.py
@@ -3,6 +3,25 @@ import re
 import nltk
 import sys
 import getopt
+import os
+import pickle
+
+
+# higher number indicates lower precendence --> i.e. when checking for precedence, we can do
+#       PRECEDENCE["OR"] > PRECEDENCE["()"] --> FALSE, meaning "OR" has lower precendence than ()
+PRECEDENCE = {
+    "()": 4,
+    "NOT": 3,
+    "AND": 2,
+    "OR": 1
+
+}
+
+def parse_query():
+    
+    parsed_query = ""
+    return parsed_query
+
 
 def usage():
     print("usage: " + sys.argv[0] + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results")
@@ -13,6 +32,63 @@ def run_search(dict_file, postings_file, queries_file, results_file):
     perform searching on the given queries file and output the results to a file
     """
     print('running search on the queries...')
+
+    
+    ''' ==================================================================
+    load in entire dictionary.txt
+    underlying assumption - dictionary is relatively small, the posting list is the large one
+    ================================================================== ''' 
+    f_dict = open(
+        os.path.join(os.path.dirname(__file__), dict_file), "rb"
+    )
+    
+    dictionary = pickle.load(f_dict)
+
+
+    ''' ==================================================================
+    load in entire queries.txt
+    save as a list, strip encoding
+    ================================================================== ''' 
+    f_queries = open(
+        os.path.join(os.path.dirname(__file__), queries_file), "rb"
+    )
+    
+    queries = [query.decode() for query in f_queries.read().splitlines()]
+
+
+
+    
+    ''' ==================================================================
+    save out to results.txt
+    just overwrite the entire file 
+    ================================================================== ''' 
+    f_results = open(
+        os.path.join(os.path.dirname(__file__), results_file), 'w'
+    )
+    
+    results = [[12, 40, 45], [], [123, 456]]
+    
+    for result in results:
+        if len(result) == 0:
+            f_results.write("\n")    
+        else:
+            listToStr = ' '.join([str(doc_id) for doc_id in result])
+            
+            f_results.write(f"{listTo}\n")
+        
+        f_results.write(line)
+        f_results.write("\n")
+    
+    f_results.close()
+
+    f_results = open(
+        os.path.join(os.path.dirname(__file__), results_file), 'r'
+    )
+
+    queries = [query for query in f_results.read().splitlines()]
+    
+
+
     # This is an empty method
     # Pls implement your code in below
 
